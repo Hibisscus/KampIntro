49c32e3c5d1e04ec9a020cea65986245e342ca2b
can <sezer@DESKTOP-ILKQKQ0>
2021-12-16 19:43:13 +0300

    12.12.2021 3.C# Kampı Ders

---
+++ Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+
+            List<string> names1 = new List<string> { "Can", "Güray", "Sezer", "Ben" };
+
+            Console.WriteLine(names1[0]);
+            Console.WriteLine(names1[1]);
+            Console.WriteLine(names1[2]);
+            Console.WriteLine(names1[3]);
+            names1.Add("YineBen");
+            Console.WriteLine(names1[0]);
+            Console.WriteLine(names1[4]);
+        }
+
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace GenericsIntro
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+         
+            MyList<string> names = new MyList<string>();
+            names.Add("Ben");
+            
+
+            
+        }
+
+    }
+}
+
